ba2a6a2da5c44268df9882ba7ff80cf3603bbf52
Patrick Fien <pmfien99@icloud.com>
2025-02-20 14:36:03 -0500

    updated logic to avoid SSR problems

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,25 @@ import { useRef, useEffect } from 'react';
 import * as deepar from 'deepar';
 
 export default function Home() {
-  const canvasRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const initializeDeepAR = async () => {
+    const initializeDeepAR = async () => {
+      const previewCanvas = document.getElementById('testing');
+      console.log(previewCanvas);
+
+      if (previewCanvas) {
         try {
           const deepAR = await deepar.initialize({
             licenseKey: '098c0d38bada8b5db7197325fa6f0ff5833227c5e4cc433e627c2d9e0651a8a5d017b403aa9efaaa',
-            previewElement: canvasRef.current as HTMLElement, // Type assertion to ensure it's not null
+            previewElement: previewCanvas as HTMLElement, 
             effect: '/filters/aviators',
             additionalOptions: {
               cameraConfig: {
-                facingMode: 'environment', // Attempt to use the back camera
+                facingMode: 'environment',
               }
             }
           });
 
-          // Ensure the camera is available and handle any errors
           const hasCameraAccess = await navigator.mediaDevices.getUserMedia({ video: true })
             .then(() => true)
             .catch(() => false);
@@ -32,16 +33,16 @@ export default function Home() {
         } catch (error) {
           console.error('Failed to initialize DeepAR:', error);
         }
-      };
+      }
+    };
 
-      initializeDeepAR();
-    }
+    initializeDeepAR();
   }, []);
 
   return (
     <div className="items-center justify-items-center h-screen w-screen">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full h-full">
-        <div id="testing" ref={canvasRef} className="w-full h-full">
+        <div id="testing" className="w-full h-full">
           {/* Canvas content */}
         </div>
       </main>
